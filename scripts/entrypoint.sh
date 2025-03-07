bf6e8f62bea2636e00cd78c92f4a354a8f2ade65
Magnus Mengelbier <49205696+mmengelbier@users.noreply.github.com>
2025-03-07 14:45:52 +0100

    Added entrypoint

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#
+# Container entrypoint for container services
+#
+#
+#
+
+
+# -- background services
+
+cd /opt/openapx/apps/rcxservice
+
+# - API
+R --no-echo --no-restore --no-save -e "rcx.service::start( port = 7749 ) &
+
+
+# -- foreground keep-alive service
+nginx -g 'daemon off;'
