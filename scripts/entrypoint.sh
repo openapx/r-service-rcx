60f76c70d4a98e3776e53456b79370a5f4a9467f
Magnus Mengelbier <49205696+mmengelbier@users.noreply.github.com>
2025-03-07 14:59:52 +0100

    Corrected unbalanced quotes in entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,7 +12,7 @@
 cd /opt/openapx/apps/rcxservice
 
 # - API
-R --no-echo --no-restore --no-save -e "rcx.service::start( port = 7749 ) &
+R --no-echo --no-restore --no-save -e "rcx.service::start( port = 7749 )" &
 
 
 # -- foreground keep-alive service
