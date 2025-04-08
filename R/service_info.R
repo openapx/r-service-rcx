8be2015aba4449de7d3ee0204e5e56ee71d48b1a
mmengelbier <magnus.mengelbier@gmail.com>
2025-04-08 10:34:23 +0000

    Implemented service information layer

--- R/service_info.R
+++ R/service_info.R
@@ -8,27 +8,37 @@ service_info <- function() {
   
   # -- initiate list
   info <- list()
+
   
+  # -- add container information
   
-  # -- add details for system info
+  info[["container"]] <- list()
   
-  info[["sys.info"]] <- as.list( Sys.info() )
+  if ( file.exists( "/opt/openapx/container-id" ) )
+    info[["container"]][["id"]] <- base::readLines( "/opt/openapx/container-id" )
   
-  
+  if ( file.exists( "/opt/openapx/container-provenance" ) )
+    info[["container"]][["provenance"]] <- base::readLines( "/opt/openapx/container-provenance" )
+
+
   # -- add details on R
   
   # - basic identifiable
-  info[["R"]] <- append( list( "version" = paste( R.Version()[ c( "major", "minor") ], collapse = "." )),
-                         R.Version()[ c( "platform", "arch") ] )
+  info[["R"]] <- list( "version" = system( "Rscript -e \"cat( paste( R.Version()[ c( \\\"major\\\", \\\"minor\\\") ], collapse = \\\".\\\" ), sep = \\\"\\\")\"", intern = TRUE ),
+                       "platform" = system( "Rscript -e \"cat( R.Version()[[ \\\"platform\\\" ]], sep = \\\"\\\" )\"", intern = TRUE ), 
+                       "architecture" = system( "Rscript -e \"cat( R.Version()[[ \\\"arch\\\" ]], sep = \\\"\\\" )\"", intern = TRUE ) )
   
   
   # - packages
   
-  installed <- installed.packages()[, "Version"]
+  installed <- unlist( strsplit( system( "Rscript -e \"cat( paste( installed.packages()[, \\\"Package\\\" ], installed.packages()[, \\\"Version\\\" ], sep = \\\"/\\\"), sep = \\\"|\\\" )\"", intern = TRUE ), 
+                                 "|", fixed = TRUE ) )
   
-  info[["packages"]] <- lapply( sort(names(installed)), function(x) {
-    list( "name" = x, "version" = unname(installed[x]) )
-  } ) 
+  
+  info[["packages"]] <- lapply( sort(installed), function(x) {
+    list( "name" = gsub( "^(.*)/.*$", "\\1", x, perl = TRUE), 
+          "version" = gsub( "^.*/(.*)$", "\\1", x, perl = TRUE) )
+  }) 
   
   
   return(info)
